0b27e8af169d61d1c07374215db7efaadc4ff6bb
altoliaw <altoliaw@hotmail.com>
2021-06-14 11:56:12 +0800

    Commit

---
+++ Demo/chex1_1.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace BS {
+    public partial class Test : System.Web.UI.Page {
+        protected void Page_Load(object sender, EventArgs e) {
+
+        }
+
+        protected void lbn_Car_Click(object sender, EventArgs e) {
+            lb_Sub.Text = "This is a book.";
+        }
+    }
+}
---
+++ Demo/chex1_2M.Master.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace BS {
+    public partial class Main : System.Web.UI.MasterPage {
+        protected void Page_Load(object sender, EventArgs e) {
+
+        }
+
+        protected void lbn_Car_Click(object sender, EventArgs e) {
+            lb_Sub.Text = "This is a book.";
+        }
+    }
+}
